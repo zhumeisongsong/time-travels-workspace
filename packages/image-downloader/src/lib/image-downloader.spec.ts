2b9669d4df0972833b1de7316fbecbbb29860f17
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-10 23:15:36 +0900

    feat: ✨ delete useless packages

--- packages/image-downloader/src/lib/image-downloader.spec.ts
+++
@@ -1,7 +0,0 @@
-import { imageDownloader } from './image-downloader';
-
-describe('imageDownloader', () => {
-  it('should work', () => {
-    expect(imageDownloader()).toEqual('image-downloader');
-  });
-});
