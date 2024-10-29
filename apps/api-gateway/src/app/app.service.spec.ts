0a7f7f5b2177c0769bd38700db3236f55312438c
zhumeisongsong <zhumeisongsong@gmail.com>
2024-10-29 14:12:50 +0900

    test: 🧪 pass test error

--- apps/api-gateway/src/app/app.service.spec.ts
+++ apps/api-gateway/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+    it('should return "Hello Gateway"', () => {
+      expect(service.getData()).toEqual({ message: 'Hello Gateway' });
     });
   });
 });
