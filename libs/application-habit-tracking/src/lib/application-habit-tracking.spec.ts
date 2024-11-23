3223cb7defc18280822e1f19666ed5c3a2d8a001
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-23 09:49:42 +0900

    feat: ✨ delete application-habit-tracking

--- libs/application-habit-tracking/src/lib/application-habit-tracking.spec.ts
+++
@@ -1,7 +0,0 @@
-import { applicationHabitTracking } from './application-habit-tracking';
-
-describe('applicationHabitTracking', () => {
-  it('should work', () => {
-    expect(applicationHabitTracking()).toEqual('application-habit-tracking');
-  });
-});
