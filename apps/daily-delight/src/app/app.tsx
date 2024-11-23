9e34e651885779e4d1080e7da90facbed8601433
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-23 22:35:50 +0900

    refactor: ♻️ remove unused import

--- apps/daily-delight/src/app/app.tsx
+++ apps/daily-delight/src/app/app.tsx
@@ -1,14 +1,12 @@
-import styled from '@emotion/styled';
-import { Route, Routes, Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
-import routes from "~react-pages";
+import routes from '~react-pages';
 
 const router = createBrowserRouter(routes);
 
 export const App = () => {
-  return (
-    <>
-     <RouterProvider router={router} />
-    </>
-  );
-}
+  return <RouterProvider router={router} />;
+};
