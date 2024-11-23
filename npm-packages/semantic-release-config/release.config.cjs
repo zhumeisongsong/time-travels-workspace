dc95ab16d87e9cdec9125199db60768c3af6b066
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-23 11:31:50 +0900

    feat: ✨ rename to semantic-release-config

--- npm-packages/semantic-release-config/release.config.cjs
+++ npm-packages/semantic-release-config/release.config.cjs
@@ -1,6 +1,6 @@
 const { createReleaseConfig } = require('./index.cjs');
 
-const name = 'semantic-release-npm-github-publish-config';
+const name = 'semantic-release-config';
 const srcRoot = `npm-packages/${name}`;
 
 module.exports = createReleaseConfig(name, srcRoot);
