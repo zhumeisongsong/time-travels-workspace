bee1e6372a6e6a5029031fdf1d5e790f26fad02e
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-23 10:30:03 +0900

    feat: ✨ add semantic-release-npm-github-publish-config package

---
+++ npm-packages/semantic-release-npm-github-publish-config/index.cjs
@@ -0,0 +1,29 @@
+export const createReleaseConfig = (name, srcRoot) => {
+  return {
+    extends: 'semantic-release-npm-github-publish',
+    pkgRoot: `${srcRoot}`,
+    tagFormat: `${name}-v\${version}`,
+    commitPaths: [`${srcRoot}/*`],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: `${srcRoot}/CHANGELOG.md`,
+        },
+      ],
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+          message:
+            `release(version): Release ${name} ` +
+            '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ],
+  };
+};
