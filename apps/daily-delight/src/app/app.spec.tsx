41f6af33009532c6fef8b3caeffce0bfdcbcc6a1
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-24 20:42:34 +0900

    test: 🧪 app test cases

--- apps/daily-delight/src/app/app.spec.tsx
+++ apps/daily-delight/src/app/app.spec.tsx
@@ -1,10 +1,22 @@
-import { render } from '@testing-library/react';
-
-import { App } from './app';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
+
+  it('should render TaskPage by default', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getByRole('button', { name: /click me/i })).toBeDefined();
   });
 });
