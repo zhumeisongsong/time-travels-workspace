1af634dedb4f8f7890fea2e35b98c564a518f160
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-24 16:09:43 +0900

    feat: ✨ import global css

--- apps/daily-delight/src/main.tsx
+++ apps/daily-delight/src/main.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 
+import '@shared/ui/styles/globals.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
