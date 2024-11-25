3178f964a8c20193586b92cfa94db4f810dc22b6
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-25 13:30:28 +0900

    feat: ✨ add CompleteTaskUseCase

---
+++ libs/modules/tasks/application/src/lib/complete-task.use-case.ts
@@ -0,0 +1,23 @@
+import { TaskRepository } from '@tasks/domain';
+import { TasksService } from './tasks.service';
+
+export class CompleteTaskUseCase {
+  private tasksRepository: TaskRepository;
+  private tasksService: TasksService;
+
+  constructor(tasksRepository: TaskRepository, tasksService: TasksService) {
+    this.tasksRepository = tasksRepository;
+    this.tasksService = tasksService;
+  }
+
+  async execute(taskHistoryId: string): Promise<void> {
+    const taskHistory = await this.tasksRepository.updateTaskHistory({
+      isCompleted: true,
+      taskHistoryId,
+    });
+
+    if (taskHistory) {
+      this.tasksService.setTask(taskHistory);
+    }
+  }
+}
