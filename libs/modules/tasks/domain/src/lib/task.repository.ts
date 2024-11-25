16c71e089389ffcb50ab9d8514caa703db8845df
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-25 11:18:31 +0900

    refactor: ♻️ Extract DTO interfaces
    for better reusability and maintainability.

--- libs/modules/tasks/domain/src/lib/task.repository.ts
+++ libs/modules/tasks/domain/src/lib/task.repository.ts
@@ -1,14 +1,22 @@
 import { TaskHistoryEntity } from './task-history.entity';
 import { TaskEntity } from './task.entity';
 
+interface CreateTaskHistoryDto {
+  taskId: string;
+  userId: string;
+}
+
+interface UpdateTaskHistoryDto {
+  taskHistoryId: string;
+  isCompleted: boolean;
+}
+
 export interface TaskRepository {
   getRandomTask(): Promise<TaskEntity | null>;
-  createTaskHistory(createTaskDto: {
-    taskId: string;
-    userId: string;
-  }): Promise<TaskHistoryEntity | null>;
-  updateTaskHistory(updateTaskDto: {
-    taskHistoryId: string;
-    isCompleted: string;
-  }): Promise<TaskHistoryEntity | null>;
+  createTaskHistory(
+    createTaskDto: CreateTaskHistoryDto,
+  ): Promise<TaskHistoryEntity | null>;
+  updateTaskHistory(
+    updateTaskDto: UpdateTaskHistoryDto,
+  ): Promise<TaskHistoryEntity | null>;
 }
