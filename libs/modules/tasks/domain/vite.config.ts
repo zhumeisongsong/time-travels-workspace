90f83e420a165634b2b27b15d24065903f40c6f8
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-25 11:12:44 +0900

    fix: 🐛 Buildable libs cannot relate on non-buildablle-libs

--- libs/modules/tasks/domain/vite.config.ts
+++ libs/modules/tasks/domain/vite.config.ts
@@ -20,29 +20,6 @@ export default defineConfig({
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: '../../../../dist/libs/modules/tasks/domain',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'tasks-domain',
-      fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: [],
-    },
-  },
   test: {
     watch: false,
     globals: true,
