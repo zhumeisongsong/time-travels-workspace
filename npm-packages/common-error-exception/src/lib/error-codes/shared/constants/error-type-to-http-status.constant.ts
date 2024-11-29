68485b1ac01be0b62a6d22a5e3f81b9915fa47a4
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-11-29 15:38:34 +0900

    feat: ✨ add errorTypeToHttpStatus

---
+++ npm-packages/common-error-exception/src/lib/error-codes/shared/constants/error-type-to-http-status.constant.ts
@@ -0,0 +1,32 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { ApplicationErrorType } from './application-error-type.constant';
+import { EntityErrorType } from './entity-error-type.constant';
+import { EntityAttributeErrorType } from './entity-attribute-error-type.constant';
+import { InfrastructureErrorType } from './infrastructure-error-type.constant';
+
+export type ErrorType =
+  | ApplicationErrorType
+  | EntityErrorType
+  | EntityAttributeErrorType
+  | InfrastructureErrorType;
+
+export const errorTypeToHttpStatus: Record<
+  ErrorType,
+  { httpStatus: StatusCodes }
+> = {
+  // application error
+  UNAUTHORIZED: { httpStatus: StatusCodes.UNAUTHORIZED },
+  NOT_ALLOWED_REQUEST: { httpStatus: StatusCodes.BAD_REQUEST },
+  // entity error
+  DUPLICATED: { httpStatus: StatusCodes.CONFLICT },
+  NOT_FOUND: { httpStatus: StatusCodes.NOT_FOUND },
+  // attribute error
+  REQUIRED: { httpStatus: StatusCodes.BAD_REQUEST },
+  INVALID_FORMAT: { httpStatus: StatusCodes.BAD_REQUEST },
+  INVALID_VALUE: { httpStatus: StatusCodes.BAD_REQUEST },
+  SIZE_MAX_EXCEEDED: { httpStatus: StatusCodes.BAD_REQUEST },
+  SIZE_MIN_LESS_THAN_ZERO: { httpStatus: StatusCodes.BAD_REQUEST },
+  LENGTH_MAX_EXCEEDED: { httpStatus: StatusCodes.BAD_REQUEST },
+  PARSE_EXCEPTION: { httpStatus: StatusCodes.BAD_REQUEST },
+};
