673e45dfbc98da356cb740e5d1836c03b73cd667
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-14 22:24:10 +0900

    fix: 🐛 delete aws cognito region error file

--- npm-packages/common-error-exception/src/lib/error-codes/infrastrcture/aws-cognito/aws-cognito.error.ts
+++
@@ -1,4 +0,0 @@
-import { infrastructureErrorType } from "../../shared/constants/infrastructure-error-type.constant";
-import { generateInfrastructureErrorObject } from "../../shared/utils/generate-infrastructure-error-object.util";
-
-const region = 'AWS_COGNITO';
