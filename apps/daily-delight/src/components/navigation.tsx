91e71c89b5370cbcd4e4ed87ca8f3a0adf05719c
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-17 22:47:28 +0900

    feat: ✨ add link to logo

--- apps/daily-delight/src/components/navigation.tsx
+++ apps/daily-delight/src/components/navigation.tsx
@@ -1,13 +1,19 @@
+import { ThemeModeToggle } from '@shared/ui/theme-mode-toggle';
 import { Sprout } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export const Navigation: React.FC = () => {
   return (
-    <header className="h-12 flex items-center justify-between">
-      <div className="max-w-screen-lg w-full mx-auto px-4">
-        <div className="flex items-center gap-2">
-          <Sprout strokeWidth={1.75} />
-          <div>Daily Delight</div>
-        </div>
+    <header className="h-16 flex items-center justify-between">
+      <div className="max-w-screen-sm w-full mx-auto px-4 flex justify-between items-center">
+        <Link to="/" className="hover:text-purple-500">
+          <div className="flex items-center gap-2">
+            <Sprout strokeWidth={1.8} />
+            <div className="text-xl font-bold">Daily Delight</div>
+          </div>
+        </Link>
+
+        <ThemeModeToggle />
       </div>
     </header>
   );
