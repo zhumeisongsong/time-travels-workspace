f160252a8196b0b703292cfc5756dd8f3c8c5c64
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-18 11:52:52 +0900

    fix: 🐛 theme icons are reversed

--- libs/shared/ui/src/ui/theme-mode-toggle.tsx
+++ libs/shared/ui/src/ui/theme-mode-toggle.tsx
@@ -13,12 +13,8 @@ export function ThemeModeToggle() {
 
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      {theme === 'light' && (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      )}
-      {theme === 'dark' && (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      )}
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
