96f75ad3fb8563e121f2659d45c057d6a3632960
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-18 12:05:11 +0900

    fix: 🐛 button icon size

--- libs/shared/ui/src/ui/theme-mode-toggle.tsx
+++ libs/shared/ui/src/ui/theme-mode-toggle.tsx
@@ -13,8 +13,10 @@ export function ThemeModeToggle() {
 
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {/* Make content visible only to screen readers while hiding it visually from sighted users.  */}
+      {/* This is an important accessibility (a11y) feature. */}
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
